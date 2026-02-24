a932266bc762b80f292e5d89a0e2405eaf12d99b
Alicasto <anixkyle@gmail.com>
2026-02-24 11:59:27 +0100

    Add project files.

---
+++ build/.qt/QtDeployTargets-Debug.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ImageViewer_FILE C:/Users/HP/Desktop/ImageViewer_template/build/Debug/ImageViewer.exe)
+set(__QT_DEPLOY_TARGET_ImageViewer_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ImageViewer_RUNTIME_DLLS C:/Qt/6.10.2/msvc2022_64/bin/Qt6Widgetsd.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Guid.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Cored.dll)
---
+++ build/.qt/QtDeployTargets-RelWithDebInfo.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ImageViewer_FILE C:/Users/HP/Desktop/ImageViewer_template/build/RelWithDebInfo/ImageViewer.exe)
+set(__QT_DEPLOY_TARGET_ImageViewer_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ImageViewer_RUNTIME_DLLS C:/Qt/6.10.2/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/msvc2022_64/bin/Qt6Core.dll)
